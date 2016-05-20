9e692696c6d7af3c6c328cb40e75f059b827bfa1
Doug Rabson <dfr@rabson.org>
2016-05-20 18:20:13 +0100

    Add TPS report

--- nfsd/threadpool.h
+++ nfsd/threadpool.h
@@ -7,7 +7,9 @@ class ThreadPool
 {
 public:
     ThreadPool(int workerCount)
-        : pending_(0)
+        : pending_(0),
+          transactions_(0),
+          lastReportTime_(std::chrono::system_clock::now())
     {
         for (int i = 0; i < workerCount; i++)
             workers_.emplace_back(this, i);
@@ -30,6 +32,17 @@ public:
 
     void dispatch(oncrpc::CallContext&& ctx)
     {
+        transactions_++;
+        if (transactions_ == 10000) {
+            using namespace std::chrono;
+            auto now = system_clock::now();
+            auto deltaTime = duration_cast<microseconds>(
+                now - lastReportTime_);
+            LOG(INFO) << "TPS: " << transactions_ / (deltaTime.count() / 1e6);
+            transactions_ = 0;
+            lastReportTime_ = now;
+        }
+
         auto p = services_.find(std::make_pair(ctx.prog(), ctx.vers()));
         if (p == services_.end()) {
             // This should not happen - the available services are all
@@ -61,29 +74,7 @@ public:
             : pool_(pool),
               id_(id)
         {
-            thread_ = std::thread(
-                [this]() {
-                    std::unique_lock<std::mutex> lock(mutex_);
-                    while (!stopping_) {
-                        VLOG_EVERY_N(2, 100) << "worker " << id_
-                                             << ": queue size "
-                                             << work_.size();
-                        if (work_.size() > 0) {
-                            auto ctx = std::move(work_.front());
-                            work_.pop_front();
-                            lock.unlock();
-                            //std::this_thread::sleep_for(10ms);
-                            VLOG_EVERY_N(2, 100) << "worker " << id_
-                                                 << ": xid: " << ctx.msg().xid
-                                                 << ": running";
-                            ctx();
-                            pool_->pending_ -= ctx.size();
-                            lock.lock();
-                        }
-                        if (work_.size() == 0)
-                            cv_.wait(lock);
-                    }
-                });
+            thread_ = std::thread(std::bind(&Worker::run, this));
         }
 
         ~Worker()
@@ -91,6 +82,34 @@ public:
             stopping_ = true;
             cv_.notify_one();
             thread_.join();
+        }
+
+        void run()
+        {
+            std::unique_lock<std::mutex> lock(mutex_);
+            while (!stopping_) {
+#if 0
+                VLOG_EVERY_N(2, 100) << "worker " << id_
+                                     << ": queue size "
+                                     << work_.size();
+#endif
+                if (work_.size() > 0) {
+                    auto ctx = std::move(work_.front());
+                    work_.pop_front();
+                    lock.unlock();
+                    //std::this_thread::sleep_for(10ms);
+#if 0
+                    VLOG_EVERY_N(2, 100) << "worker " << id_
+                                         << ": xid: " << ctx.msg().xid
+                                         << ": running";
+#endif
+                    ctx();
+                    pool_->pending_ -= ctx.size();
+                    lock.lock();
+                }
+                if (work_.size() == 0)
+                    cv_.wait(lock);
+            }
         }
 
         void add(oncrpc::CallContext&& ctx)
@@ -112,6 +131,8 @@ public:
     std::deque<Worker> workers_;
     int nextWorker_ = 0;
     std::atomic_int pending_;
+    std::atomic_int transactions_;
+    std::chrono::system_clock::time_point lastReportTime_;
     std::unordered_map<std::pair<uint32_t, uint32_t>,
         oncrpc::Service> services_;
 };
