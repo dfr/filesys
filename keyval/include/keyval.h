db50a79d28bec8a587e7cb460220719175a4dd2e
Doug Rabson <dfr@rabson.org>
2016-10-24 10:24:15 +0100

    Add documentation

--- keyval/include/keyval.h
+++ keyval/include/keyval.h
@@ -24,13 +24,22 @@ class Database
 {
 public:
     virtual ~Database() {}
+
+    /// Get a pointer to the given namespace object
     virtual std::shared_ptr<Namespace> getNamespace(
         const std::string& name) = 0;
+
+    /// Start a new transaction
     virtual std::unique_ptr<Transaction> beginTransaction() = 0;
+
+    /// Commit the transaction to the database
     virtual void commit(std::unique_ptr<Transaction>&& transaction) = 0;
+
+    /// Flush any commit transactions to stable storage
     virtual void flush() = 0;
 };
 
+/// Key/value pairs are grouped by namespace
 class Namespace
 {
 public:
@@ -43,11 +52,16 @@ public:
     /// Create a new iterator which is set to iterate from the given key
     virtual std::unique_ptr<Iterator> iterator(std::shared_ptr<Buffer> key) = 0;
 
+    /// Get the value for a given key in this namespace
     virtual std::shared_ptr<Buffer> get(std::shared_ptr<Buffer> key) = 0;
+
+    /// Return an approximate indication of the space used by this namespace
     virtual std::uint64_t spaceUsed(
         std::shared_ptr<Buffer> start, std::shared_ptr<Buffer> end) = 0;
 };
 
+/// Iterator objects are used to iterate through the key/value pairs
+/// in a namespace
 class Iterator
 {
 public:
@@ -82,20 +96,29 @@ public:
     virtual std::shared_ptr<Buffer> value() const = 0;
 };
 
+/// A set of write operations to a database which must all be executed
+/// together atomically
 class Transaction
 {
 public:
     virtual ~Transaction() {}
+
+    /// Write a new value for a key in the given namespace
     virtual void put(
         std::shared_ptr<Namespace> ns,
         std::shared_ptr<Buffer> key,
         std::shared_ptr<Buffer> val) = 0;
+
+    /// Remove a key/value pair from the given namespace
     virtual void remove(
         std::shared_ptr<Namespace> ns,
         std::shared_ptr<Buffer> key) = 0;
 };
 
+/// Create an in-memory database - typically used for unit tests
 std::shared_ptr<Database> make_memdb();
+
+/// Create a database backed by RocksDB
 std::shared_ptr<Database> make_rocksdb(const std::string& filename);
 
 }
